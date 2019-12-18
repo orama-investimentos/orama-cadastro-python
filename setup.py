c63328c6872af44cabbff75de0f2a8adb4f08073
Marcelo Lino <mdslino@gmail.com>
2019-12-18 09:24:53 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "cadastro-orama"
-VERSION = "1.0.3rc2"
+VERSION = "1.0.3.rc2"
 # To install the library, run the following
 #
 # python setup.py install
